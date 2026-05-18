ad4179d002cdc566475f3d4bdb0982576aee2f21
Noémie Niamien <noemienmn@gmail.com>
2026-05-19 01:08:11 +0200

    feat:localisation

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+<p><strong>Mar - Ven :</strong> 13h - 05h</p>
+                <p><strong>Sam - Dim :</strong> 10h - 07h</p>
+
+
+                div class="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d178239.86103686353!2d-3.994885241577522!3d5.2851986869851135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfc1eebbf667104b%3A0xdf16e1418c0b9e6c!2sRue%20Simon%20Deli%2C%20Abidjan%2C%20C%C3%B4te%20d&#39;Ivoire!5e0!3m2!1sfr!2sfr!4v1779111059767!5m2!1sfr!2sfr" width="300" height="350" style="border:5;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
