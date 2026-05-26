9ac622438e10f2a4abf32a5af72252c2d6d6edc2
Daniel BOKINGO <bokingodaniel4@gmail.com>
2026-05-26 14:52:43 +0200

    feat/search-bar

--- index.php
+++ index.php
@@ -13,8 +13,8 @@ $services = $result->fetchAll();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chichats - Réservation de table</title>
-    <link rel="stylesheet" href="../style.css">
-    <link rel="stylesheet" href="style.css">
+    
+    <link rel="stylesheet" href="/CSS/style.css">
 </head>
 <body>
     <header class="hero">
