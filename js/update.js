00adeb87295c463a3726f6a167e032e0202414fa
Basile Monier <basilemonier@gmail.com>
2026-05-27 09:51:49 +0200

    feat: modification des données clients au sein du formulaire de update'

--- js/update.js
+++ js/update.js
@@ -1,16 +1,14 @@
-function ouvrirModale(id, nom, prenom, date, heure) { 
+function ouvrirModale(id, nom, date, heure) {
 
-            
-            document.getElementById('input-id').value     = id;
-            document.getElementById('input-nom').value    = nom;
-            document.getElementById('input-date').value   = date;
-            document.getElementById('input-heure').value  = heure;
-            document.getElementById('modale').style.display  = 'block';
-            document.getElementById('overlay').style.display = 'block';
-        }
+    document.getElementById('input-id').value    = id;
+    document.getElementById('input-nom').value   = nom;
+    document.getElementById('input-date').value  = date;  
+    document.getElementById('input-heure').value = heure.substring(0, 5);
+    document.getElementById('modale').style.display  = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
 
-        function fermerModale() {
-            
-            document.getElementById('modale').style.display  = 'none';
-            document.getElementById('overlay').style.display = 'none';
-        }
+function fermerModale() {
+    document.getElementById('modale').style.display  = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
